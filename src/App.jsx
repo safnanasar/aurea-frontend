a49ce7f886830777bbb2e4919be140b7cc3febe4
Safna <ashhadharun@gmail.com>
2026-05-29 12:13:57 +0530

    "maid alignment"

--- src/App.jsx
+++ src/App.jsx
@@ -24,29 +24,13 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <Home
-                            user={user}
-                            onLogout={handleLogout}
-                            cart={cart}
-                            setCart={setCart}
-                        />
+                <Route path="/"element={
+                        <Home user={user} onLogout={handleLogout}cart={cart}setCart={setCart}/>
                     }
                 />
-
                 <Route path="/register" element={<Register />} />
-
-                <Route
-                    path="/login"
-                    element={<Login setUser={setUser} />}
-                />
-
-                <Route
-                path="/cart"
-                element={<Cart cart={cart} setCart={setCart} />}
-                />
+                <Route path="/login" element={<Login setUser={setUser} />}/>
+                <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />}/>
                 <Route path="/payment" element={<Payment cart={cart} setCart={setCart} />}/>
             </Routes>
         </BrowserRouter>
