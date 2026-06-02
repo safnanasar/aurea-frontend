c82d906063808689d4cc08322cfaf64f993d8c21
Safna <ashhadharun@gmail.com>
2026-06-02 19:04:09 +0530

    "removed code"

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css'
 
 function App() {
     const [user, setUser] = useState(
-        localStorage.getItem('currentUser') || null
+        localStorage.getItem('currentUser') 
     )
 
     const [cart, setCart] = useState([])
